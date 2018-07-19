449bdefcf89d8d27214ae53ff225f4b91a8a915b
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-07-19 12:59:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -7,7 +9,28 @@ class Song
   end
 
   def save
-    self.class.all << self
+    @@all << self
   end
 
+  def self.create
+    new_song = Song.new
+    new_song.save
+    new_song
+  end
+
+  def self.new_by_name(value)
+    new_song = Song.new
+    new_song.name = value
+    new_song
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def alphbetical
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    @@all.sort_by{|song| song.name.downcase.split().map{|x| alphabet.index[x] }}
+  end
+binding.pry
 end
